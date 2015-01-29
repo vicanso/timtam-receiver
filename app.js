12b0e0e234567071d08d62b655926c7c28b49fe5
vicanso <vicansocanbico@gmail.com>
2015-01-29 22:32:36 +0800

    commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var server = require('./lib/server');
 var jtLogger = require('jtlogger');
 jtLogger.appPath = __dirname + '/';
-var address = process.env.address || '127.0.0.1:2900';
 
-server.start(address);
+server.start();
