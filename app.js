809b2968d1ff6134898e16ff34b36b633a909e85
vicanso <vicansocanbico@gmail.com>
2015-09-22 21:27:23 +0800

    commit

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const _ = require('lodash');
 const fs = require('fs');
 const server = dgram.createSocket("udp4");
 const path = require('path');
-const logger = require('./lib/logger');
 const pkg = require('./package');
+const jtlogger = require('jtlogger');
 const config = require('./config');
-const ConsulClient = require('consul-simple-client');
-const co = require('co');
+if (config.env === 'production') {
+  jtlogger.init([{
+    timestamp: true
+  }, {
+    type: 'file',
+    filename: path.join('/var/log', pkg.name, 'out.log'),
+    timestamp: true
+  }]);
+}
+const logger = require('./lib/logger');
+
 if (config.env === 'production') {
   initLog();
-  co(function*() {
-    yield register();
-  }).catch(function(err) {
-    console.error(err);
-  });
 }
 
 
@@ -48,38 +52,4 @@ function initLog() {
     type: 'file',
     filename: path.join('/var/log', pkg.name, 'out.log')
   }]);
-}
-
-
-/**
- * [register 注册服务]
- * @return {[type]} [description]
- */
-function* register() {
-  let consulInfo = config.consul;
-  console.dir(consulInfo);
-  let consul = new ConsulClient({
-    host: consulInfo.hostname,
-    port: consulInfo.port
-  });
-
-  let hostName = process.env.HOSTNAME;
-  let hosts = fs.readFileSync('/etc/hosts', 'utf8');
-  // etc hosts中的ip都是正常的，因此正则的匹配考虑的简单一些
-  let reg = new RegExp('((?:[0-9]{1,3}\.){3}[0-9]{1,3})\\s*' + hostName);
-  let address = _.get(reg.exec(hosts), 1);
-  if (!address) {
-    throw new Error('can not get address');
-  }
-  let tags = ['udp-log', config.env];
-  if (config.zmq) {
-    tags.push('zmq');
-  }
-  yield consul.register({
-    id: hostName,
-    service: config.app,
-    address: address,
-    port: config.port,
-    tags: _.uniq(tags)
-  });
 }
--- config.js
+++ config.js
@@ -12,6 +12,6 @@ exports.env = process.env.NODE_ENV || 'development';
 
 exports.app = pkg.name;
 
-exports.consul = url.parse(process.env.CONSUL || 'http://127.0.0.1:8500');
-
 exports.zmqPort = process.env.ZMQ_PORT || 6010;
+
+exports.enableZmq = process.env.ZMQ !== 'disable';
